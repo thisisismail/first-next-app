f278b228364f53ecf19215eceb29f394af4d49b8
thisisismail <simail.official@gmail.com>
2022-07-23 13:39:29 +0700

    1-responsive-try

--- pages/posts.js
+++ pages/posts.js
@@ -17,9 +17,10 @@ export default function posts({ dataPosts, dataUsers }) {
     return (
       <Card
         key={item.id}
+        style={{ width: 500 }}
         className="border-2 rounded-xl w-full flex flex-col justify-center"
       >
-        <CardBody>
+        <CardBody className="gap-3 flex flex-col">
           <h1 className="font-bold text-2xl line-clamp-2 overflow-hidden">
             {item.title}
           </h1>
@@ -27,7 +28,6 @@ export default function posts({ dataPosts, dataUsers }) {
         </CardBody>
         <CardFooter divider className="">
           <h1>{userInfo?.name}</h1>
-          <h1>{userInfo?.email}</h1>
         </CardFooter>
       </Card>
     );
@@ -37,8 +37,8 @@ export default function posts({ dataPosts, dataUsers }) {
     <Layout>
       <div>Posts</div>
       <div
-        className="border-0 mx-4 flex flex-col gap-4"
-        style={{ maxWidth: 500 }}
+        className="border-2 gap-4 flex flex-wrap justify-center mx-auto px-4"
+        style={{ maxWidth: 1200 }}
       >
         {postsList}
       </div>
