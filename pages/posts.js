b34ec281086a9a56125515f4543a4b9cd662192e
thisisismail <simail.official@gmail.com>
2022-07-23 15:47:53 +0700

    parsing-posts-w-comments

--- pages/posts.js
+++ pages/posts.js
@@ -1,7 +1,8 @@
 import { Card, CardBody, CardFooter } from "@material-tailwind/react";
+import { GoComment } from "react-icons/go";
 import Layout from "../components/layout/index";
 
-export default function posts({ dataPosts, dataUsers }) {
+export default function posts({ dataPosts, dataUsers, dataComments }) {
   if (!dataPosts) {
     return null;
   }
@@ -11,14 +12,34 @@ export default function posts({ dataPosts, dataUsers }) {
 
   const postsList = dataPosts?.map((item) => {
     const userId = item.user_id;
+    const postId = item.id;
     const userInfo = dataUsers?.find((user) => {
       return user.id === userId;
     });
+    const commentInfo = dataComments?.find((comment) => {
+      return comment.find((nComment) => {
+        return nComment.post_id === postId;
+      });
+    });
+    // console.log(dataComments);
+    // console.log("S =======================");
+    // console.log(commentInfo);
+    // console.log("F =======================");
+    const comments = commentInfo?.map((comment) => {
+      return (
+        <div key={comment.email} className="mt-2">
+          <h1 className="font-bold">{comment.name}</h1>
+          <h1>{comment.body}</h1>
+        </div>
+      );
+    });
+    // console.log(dataComments);
+
     return (
       <Card
         key={item.id}
-        style={{ width: 500 }}
-        className="border-2 rounded-xl w-full flex flex-col justify-center"
+        style={{ width: 300 }}
+        className="border-0 rounded-xl w-full flex flex-col justify-center"
       >
         <CardBody className="gap-3 flex flex-col">
           <h1 className="font-bold text-2xl line-clamp-2 overflow-hidden">
@@ -26,9 +47,15 @@ export default function posts({ dataPosts, dataUsers }) {
           </h1>
           <h6 className="overflow-hidden line-clamp-2 text-md">{item.body}</h6>
         </CardBody>
-        <CardFooter divider className="">
-          <h1>{userInfo?.name}</h1>
+        <CardFooter divider className="flex justify-between items-center">
+          <h1 className="border-0">{userInfo?.name}</h1>
+          <div className="border-0">
+            <GoComment size={24} className=" w-full text-black" />
+          </div>
         </CardFooter>
+        <div>Comments</div>
+        <div>{comments}</div>
+        {/* <div>{comments}</div> */}
       </Card>
     );
   });
@@ -37,7 +64,7 @@ export default function posts({ dataPosts, dataUsers }) {
     <Layout>
       <div>Posts</div>
       <div
-        className="border-2 gap-4 flex flex-wrap justify-center mx-auto px-4"
+        className="border-0 gap-4 flex flex-wrap justify-center mx-auto px-4"
         style={{ maxWidth: 1200 }}
       >
         {postsList}
@@ -63,10 +90,21 @@ export const getStaticProps = async () => {
     )
   );
 
+  const dataComments = await Promise.all(
+    dataPosts.map((result) =>
+      fetch(`https://gorest.co.in/public/v2/posts/${result.id}/comments`).then(
+        (res) => {
+          return res.json();
+        }
+      )
+    )
+  );
+
   return {
     props: {
       dataPosts,
       dataUsers,
+      dataComments,
     },
   };
 };
